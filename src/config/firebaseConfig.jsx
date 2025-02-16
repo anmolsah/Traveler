da8728361e5ffd8173954b3cbcce1ca73c98fedf
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-16 20:35:39 +0530

    refactor: clean up firebaseConfig.jsx by removing unnecessary comments

--- src/config/firebaseConfig.jsx
+++ src/config/firebaseConfig.jsx
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: Add SDKs for Firebase products that you want to use
 const firebaseConfig = {
   apiKey: "AIzaSyA4DbxTAGRO7ccs4DuEg1pkRoGpsaJxqnQ",
   authDomain: "rahi-98f41.firebaseapp.com",
